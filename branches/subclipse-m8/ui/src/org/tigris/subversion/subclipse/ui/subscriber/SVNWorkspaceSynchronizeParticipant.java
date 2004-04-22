769f1d269ec401edcf739b3ac8ad015aeb2c5dd5
mml <mml@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-04-22 20:46:38 +0000

    getting the bits i missed...
    also, last revision contained linux update to svn 1.0.2

---
+++ branches/subclipse-m8/ui/src/org/tigris/subversion/subclipse/ui/subscriber/SVNWorkspaceSynchronizeParticipant.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Apr 22, 2004
+ */
+package org.tigris.subversion.subclipse.ui.subscriber;
+
+import org.eclipse.team.ui.synchronize.subscribers.SubscriberParticipant;
+
+/**
+ * @author mml
+ *
+ */
+public class SVNWorkspaceSynchronizeParticipant extends SubscriberParticipant{
+	public static final String ID = "org.tigris.subversion.subclipse.workspace-sync-participant";
+
+	
+}
