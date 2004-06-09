5ec2cdbb5b0ce8ea13cf6d60c370a183ffd2282a
mml <mml@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-06-09 18:00:25 +0000

    added skeleton synch and compare subscribers

---
+++ branches/subclipse-3/ui/src/org/tigris/subversion/subclipse/ui/subscriber/SVNCompareParticipant.java
@@ -0,0 +1,11 @@
+package org.tigris.subversion.subclipse.ui.subscriber;
+
+import org.eclipse.team.ui.synchronize.SubscriberParticipant;
+
+/**
+ * @author mml
+ *
+ */
+public class SVNCompareParticipant extends SubscriberParticipant{
+
+}
