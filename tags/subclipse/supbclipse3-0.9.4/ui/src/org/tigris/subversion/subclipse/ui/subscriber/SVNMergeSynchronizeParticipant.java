348fad77c963af6a9c7c63244d8dfabc712cc551
mml <mml@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-06-29 03:12:21 +0000

    tagged 0.9.4

---
+++ tags/subclipse/supbclipse3-0.9.4/ui/src/org/tigris/subversion/subclipse/ui/subscriber/SVNMergeSynchronizeParticipant.java
@@ -0,0 +1,13 @@
+package org.tigris.subversion.subclipse.ui.subscriber;
+
+import org.eclipse.team.ui.synchronize.SubscriberParticipant;
+
+/**
+ * @author mml
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SVNMergeSynchronizeParticipant extends SubscriberParticipant{
+
+}
