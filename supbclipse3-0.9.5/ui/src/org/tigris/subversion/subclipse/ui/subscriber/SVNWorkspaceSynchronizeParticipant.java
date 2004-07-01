001ff0465693700e661beebdbc8fd358b1a09399
mml <mml@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-07-01 01:21:49 +0000

    0.9.5 release

---
+++ supbclipse3-0.9.5/ui/src/org/tigris/subversion/subclipse/ui/subscriber/SVNWorkspaceSynchronizeParticipant.java
@@ -0,0 +1,7 @@
+package org.tigris.subversion.subclipse.ui.subscriber;
+
+import org.eclipse.team.ui.synchronize.SubscriberParticipant;
+
+public class SVNWorkspaceSynchronizeParticipant extends SubscriberParticipant {
+
+}
