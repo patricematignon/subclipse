4099a0525c1b71835f58fd5096f8629410223a41
cchab <cchab@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-07-11 18:45:18 +0000

    * BaseFile.java, BaseFolder.java added : represent a remote file with BASE revision and remote folder with BASE revision

---
+++ branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/resources/BaseFile.java
@@ -0,0 +1,73 @@
+/*
+ * Created on 10 juil. 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.tigris.subversion.subclipse.core.resources;
+
+import java.io.InputStream;
+import java.util.Date;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.team.core.TeamException;
+import org.tigris.subversion.subclipse.core.ISVNLocalResource;
+import org.tigris.subversion.subclipse.core.ISVNRemoteFile;
+import org.tigris.subversion.subclipse.core.Policy;
+import org.tigris.subversion.subclipse.core.SVNException;
+import org.tigris.subversion.subclipse.core.util.Assert;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
+import org.tigris.subversion.svnclientadapter.SVNRevision;
+
+/**
+ * represents the base revision of a file 
+ * 
+ */
+public class BaseFile extends RemoteFile implements ISVNRemoteFile {
+	private ISVNLocalResource localResource;
+	
+	public BaseFile( 
+			ISVNLocalResource localResource,
+	        boolean hasProps,
+	        SVNRevision.Number lastChangedRevision,
+	        Date date,
+	        String author) throws SVNException {
+		super(null, localResource.getRepository(), localResource.getUrl(), SVNRevision.BASE,hasProps, lastChangedRevision, date, author);
+		Assert.isNotNull(localResource);
+		this.localResource = localResource;
+	}	
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.team.core.variants.CachedResourceVariant#fetchContents(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void fetchContents(IProgressMonitor monitor) throws TeamException {
+		// we override method because we can't get the content from the svnurl,
+		// we need to use the underlying localResource
+
+		if (monitor == null) {
+			monitor = new NullProgressMonitor();
+		}
+		monitor.beginTask(Policy.bind("RemoteFile.getContents"), 100);//$NON-NLS-1$
+		try {
+			
+				ISVNClientAdapter svnClient = repository.getSVNClient();
+				InputStream inputStream;
+				try {
+					inputStream = svnClient.getContent(localResource.getFile(),
+							getRevision());
+					super.setContents(inputStream, monitor);
+				} catch (SVNClientException e) {
+					throw new TeamException(
+							"Failed in remoteFile.getContents()", e);
+				}
+				monitor.done();
+			
+		} finally {
+			monitor.done();
+		}
+	}	
+	
+}
---
+++ branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/resources/BaseFolder.java
@@ -0,0 +1,115 @@
+/*
+ * Created on 10 juil. 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.tigris.subversion.subclipse.core.resources;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.tigris.subversion.subclipse.core.ISVNLocalResource;
+import org.tigris.subversion.subclipse.core.ISVNRemoteFolder;
+import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
+import org.tigris.subversion.subclipse.core.Policy;
+import org.tigris.subversion.subclipse.core.SVNException;
+import org.tigris.subversion.subclipse.core.SVNStatus;
+import org.tigris.subversion.subclipse.core.util.Assert;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.ISVNDirEntry;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
+import org.tigris.subversion.svnclientadapter.SVNNodeKind;
+import org.tigris.subversion.svnclientadapter.SVNRevision;
+
+/**
+ * represents the base revision of a folder
+ * 
+ */
+public class BaseFolder extends RemoteFolder implements ISVNRemoteFolder {
+	private ISVNLocalResource localResource;
+	
+	/**
+	 * Constructor for RemoteFolder.
+	 * @throws SVNException
+	 */
+	public BaseFolder( 
+			ISVNLocalResource localResource,
+	        boolean hasProps,
+	        SVNRevision.Number lastChangedRevision,
+	        Date date,
+	        String author) throws SVNException {
+		super(null, localResource.getRepository(), localResource.getUrl(), SVNRevision.BASE,hasProps, lastChangedRevision, date, author);
+		Assert.isNotNull(localResource);
+		this.localResource = localResource;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.tigris.subversion.subclipse.core.resources.RemoteFolder#getMembers(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected ISVNRemoteResource[] getMembers(IProgressMonitor monitor)
+			throws SVNException {
+		// we can't use the svnurl to get the members of a base folder, that's
+		// why we needed to override this method
+		
+		final IProgressMonitor progress = Policy.monitorFor(monitor);
+		progress.beginTask(Policy.bind("RemoteFolder.getMembers"), 100); //$NON-NLS-1$
+        
+        if (children != null)
+        {
+            progress.done();
+            return children;
+        }
+		
+		try {
+            ISVNClientAdapter client = getRepository().getSVNClient();
+				
+			ISVNDirEntry[] list = client.getList(localResource.getFile(),getRevision(),false);
+			List result = new ArrayList();
+
+			IContainer container = (IContainer)localResource.getIResource();			
+			// directories first				
+			for (int i=0;i<list.length;i++)
+			{
+                ISVNDirEntry entry = list[i];
+                if (entry.getNodeKind() == SVNNodeKind.DIR)
+				{
+				    result.add(new BaseFolder(
+	                   new LocalFolder(container.getFolder(new Path(entry.getPath()))), 
+                       entry.getHasProps(),
+                       entry.getLastChangedRevision(),
+                       entry.getLastChangedDate(),
+                       entry.getLastCommitAuthor()));
+				}
+			}
+
+			// files then				
+			for (int i=0;i<list.length;i++)
+			{
+				ISVNDirEntry entry = list[i];
+				if (entry.getNodeKind() == SVNNodeKind.FILE)
+				{
+					result.add(new BaseFile(
+                        new LocalFile(container.getFile(new Path(entry.getPath()))), 
+                        entry.getHasProps(),
+                        entry.getLastChangedRevision(),
+                        entry.getLastChangedDate(),
+                        entry.getLastCommitAuthor()));
+			     }
+					 	
+			}
+
+			children = (ISVNRemoteResource[])result.toArray(new ISVNRemoteResource[0]);
+            return children;
+        } catch (SVNClientException e)
+		{
+            throw new SVNException(new SVNStatus(SVNStatus.ERROR, SVNStatus.DOES_NOT_EXIST, Policy.bind("RemoteFolder.doesNotExist", getRepositoryRelativePath()))); //$NON-NLS-1$
+        } finally {
+			progress.done();
+		}	 	
+	}
+}
