2114ea1eecf9cd7cb09005c8ea7f7218a6a108dc
mml <mml@a34601ff-79c0-0310-90bb-fac03367fbd1>
2004-09-10 16:07:45 +0000

    0.9.19

---
+++ subclipse3-0.9.19/core/src/org/tigris/subversion/subclipse/core/SVNProjectSetCapability.java
@@ -0,0 +1,232 @@
+/**********************************************************************
+ Copyright (c) 2004 Dan Rubel and others.
+ All rights reserved.   This program and the accompanying materials
+ are made available under the terms of the Common Public License v1.0
+ which accompanies this distribution, and is available at
+ http://www.eclipse.org/legal/cpl-v10.html
+
+ Contributors:
+
+ Dan Rubel - initial API and implementation
+ Panagiotis Korros (pkorros@bigfoot.com) - modified for Subversion
+ **********************************************************************/
+
+package org.tigris.subversion.subclipse.core;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.team.core.ProjectSetCapability;
+import org.eclipse.team.core.ProjectSetSerializationContext;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.TeamException;
+import org.tigris.subversion.subclipse.core.repo.SVNRepositoryLocation;
+import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+
+/**
+ * An object for serializing and deserializing of references to SVN based
+ * projects. Given a project, it can produce a UTF-8 encoded String which can be
+ * stored in a file. Given this String, it can load a project into the
+ * workspace.
+ */
+public class SVNProjectSetCapability extends ProjectSetCapability {
+
+	/**
+	 * Override superclass implementation to return an array of project
+	 * references.
+	 * 
+	 * @see ProjectSetSerializer#asReference(IProject[],
+	 *      ProjectSetSerializationContext, IProgressMonitor)
+	 */
+	public String[] asReference(IProject[] projects,
+			ProjectSetSerializationContext context, IProgressMonitor monitor)
+			throws TeamException {
+
+		String[] result = new String[projects.length];
+		for (int i = 0; i < projects.length; i++)
+			result[i] = asReference(projects[i]);
+		return result;
+	}
+
+	/**
+	 * Answer a string representing the specified project
+	 * 
+	 * @param project
+	 *            the project (not <code>null</code>)
+	 * @return the project reference (not <code>null</code>)
+	 * @throws CVSException
+	 */
+	private String asReference(IProject project) throws TeamException {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("0.9.3,"); //$NON-NLS-1$
+
+		SVNTeamProvider provider = (SVNTeamProvider) RepositoryProvider
+				.getProvider(project);
+		SVNWorkspaceRoot root = provider.getSVNWorkspaceRoot();
+
+		buffer.append(root.getLocalRoot().getUrl().get());
+		buffer.append(",");
+		buffer.append(project.getName());
+		return buffer.toString();
+	}
+
+	/**
+	 * Override superclass implementation to load the referenced projects into
+	 * the workspace.
+	 * 
+	 * @see org.eclipse.team.core.ProjectSetSerializer#addToWorkspace(java.lang.String[],
+	 *      org.eclipse.team.core.ProjectSetSerializationContext,
+	 *      org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public IProject[] addToWorkspace(String[] referenceStrings,
+			ProjectSetSerializationContext context, IProgressMonitor monitor)
+			throws TeamException {
+
+		monitor = Policy.monitorFor(monitor);
+		Policy.checkCanceled(monitor);
+
+		// Confirm the projects to be loaded
+		Map infoMap = new HashMap(referenceStrings.length);
+		IProject[] projects = asProjects(referenceStrings, infoMap);
+		projects = confirmOverwrite(context, projects);
+		if (projects == null){
+			return new IProject[0];
+		}
+		// Load the projects
+		return checkout(projects, infoMap, monitor);
+	}
+
+	/**
+	 * Translate the reference strings into projects to be loaded and build a
+	 * mapping of project to project load information.
+	 * 
+	 * @param referenceStrings
+	 *            project references
+	 * @param infoMap
+	 *            a mapping of project to project load information
+	 * @return the projects to be loaded
+	 */
+	private IProject[] asProjects(String[] referenceStrings, Map infoMap)
+			throws SVNException {
+		Collection result = new ArrayList();
+		for (int i = 0; i < referenceStrings.length; i++) {
+			StringTokenizer tokenizer = new StringTokenizer(
+					referenceStrings[i], ","); //$NON-NLS-1$
+			String version = tokenizer.nextToken();
+			// If this is a newer version, then ignore it
+			if (!version.equals("0.9.3")){ //$NON-NLS-1$
+				continue;
+			}
+			LoadInfo info = new LoadInfo(tokenizer);
+			IProject proj = info.getProject();
+			result.add(proj);
+			infoMap.put(proj, info);
+		}
+		return (IProject[]) result.toArray(new IProject[result.size()]);
+	}
+
+	/**
+	 * Checkout projects from the SVN repository
+	 * 
+	 * @param projects
+	 *            the projects to be loaded from the repository
+	 * @param infoMap
+	 *            a mapping of project to project load information
+	 * @param monitor
+	 *            the progress monitor (not <code>null</code>)
+	 */
+	private IProject[] checkout(IProject[] projects, Map infoMap,
+			IProgressMonitor monitor) throws TeamException {
+
+		monitor.beginTask("", 1000 * projects.length); //$NON-NLS-1$
+		List result = new ArrayList();
+		try {
+			for (int i = 0; i < projects.length; i++) {
+				if (monitor.isCanceled()){
+					break;
+				}
+				IProject project = projects[i];
+				LoadInfo info = (LoadInfo) infoMap.get(project);
+				if (info != null
+						&& info.checkout(new SubProgressMonitor(monitor, 1000))){
+					result.add(project);
+				}
+			}
+		} finally {
+			monitor.done();
+		}
+		return (IProject[]) result.toArray(new IProject[result.size()]);
+	}
+
+	/**
+	 * Internal class for adding projects to the workspace
+	 */
+	class LoadInfo {
+		private final ISVNRepositoryLocation repositoryLocation;
+		private final IProject project;
+
+		/**
+		 * Construct a new instance wrappering the specified project reference
+		 * 
+		 * @param projRef
+		 *            the project reference
+		 */
+		LoadInfo(StringTokenizer tokenizer) throws SVNException {
+			String repo = tokenizer.nextToken();
+			repositoryLocation = SVNRepositoryLocation.fromString(repo);
+			String projectName = tokenizer.nextToken();
+			project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+					projectName);
+		}
+
+		/**
+		 * Answer the project referenced by this object. The project may or may
+		 * not already exist.
+		 * 
+		 * @return the project (not <code>null</code>)
+		 */
+		private IProject getProject() {
+			return project;
+		}
+
+		/**
+		 * Checkout the project specified by this reference.
+		 * 
+		 * @param monitor
+		 *            project monitor
+		 * @return true if loaded, else false
+		 * @throws TeamException
+		 */
+		boolean checkout(IProgressMonitor monitor) throws TeamException {
+			if (repositoryLocation == null){
+				return false;
+			}
+			SVNWorkspaceRoot
+					.checkout(
+							new ISVNRemoteFolder[]{(ISVNRemoteFolder) repositoryLocation
+									.getRootFolder()}, new IProject[]{project},
+							monitor);
+			return true;
+		}
+	}
+
+	private static void deepDelete(File resource) {
+		if (resource.isDirectory()) {
+			File[] fileList = resource.listFiles();
+			for (int i = 0; i < fileList.length; i++) {
+				deepDelete(fileList[i]);
+			}
+		}
+		resource.delete();
+	}
+
+}
---
+++ subclipse3-0.9.19/ui/src/org/tigris/subversion/subclipse/ui/properties/SVNPropertyPage.java
@@ -0,0 +1,236 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Daniel Bradby 
+ *******************************************************************************/
+package org.tigris.subversion.subclipse.ui.properties;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.tigris.subversion.subclipse.core.ISVNLocalResource;
+import org.tigris.subversion.subclipse.core.SVNProviderPlugin;
+import org.tigris.subversion.subclipse.core.SVNTeamProvider;
+import org.tigris.subversion.subclipse.core.resources.LocalResourceStatus;
+import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
+
+public class SVNPropertyPage extends PropertyPage {
+
+    private static final int TEXT_FIELD_WIDTH = 50;
+
+    private Text ignoredValue;
+    private Text managedValue;
+    private Text hasRemoteValue;
+    private Text urlValue;
+    private Text lastChangedRevisionValue;
+    private Text lastChangedDateValue;
+    private Text lastCommitAuthorValue;
+    private Text textStatusValue;
+    private Text mergedValue;
+    private Text deletedValue;
+    private Text modifiedValue;
+    private Text addedValue;
+    private Text revisionValue;
+    private Text copiedValue;
+    private Text urlCopiedFromValue;
+    private Text pathValue;
+
+    public SVNPropertyPage() {
+        super();
+    }
+
+    private void addFirstSection(Composite parent) {
+        Composite composite = createDefaultComposite(parent);
+
+        //Label for path field
+        Label pathLabel = new Label(composite, SWT.NONE);
+        pathLabel.setText("Path");
+
+        // Path text field
+        Text pathValueText = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+        GridData gd = new GridData();
+        gd.horizontalSpan = 2;
+        pathValueText.setLayoutData(gd);
+        pathValueText.setText(((IResource) getElement()).getFullPath().toString());
+    }
+
+    private void addSeparator(Composite parent) {
+        Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+        GridData gridData = new GridData();
+        gridData.horizontalAlignment = GridData.FILL;
+        gridData.grabExcessHorizontalSpace = true;
+        separator.setLayoutData(gridData);
+    }
+
+    private void addSecondSection(Composite parent) {
+        Composite composite = createDefaultComposite(parent);
+
+        Label label = new Label(composite, SWT.NONE);
+        label.setText("Ignored");
+        ignoredValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Managed");
+        managedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Has Remote");
+        hasRemoteValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("URL");
+        urlValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Last Changed Revision");
+        lastChangedRevisionValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Last Changed Date");
+        lastChangedDateValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Last Commit Author");
+        lastCommitAuthorValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Status");
+        textStatusValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Merged");
+        mergedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Deleted");
+        deletedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Modified");
+        modifiedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Added");
+        addedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Revision");
+        revisionValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Copied");
+        copiedValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+
+        label = new Label(composite, SWT.NONE);
+        label.setText("Path");
+
+        GridData gd = new GridData();
+        gd.horizontalSpan = 2;
+        pathValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+        pathValue.setLayoutData(gd);
+
+        // Populate owner text field
+        try {
+            IResource resource = (IResource) getElement();
+            SVNTeamProvider svnProvider = (SVNTeamProvider) RepositoryProvider.getProvider(resource
+                    .getProject(), SVNProviderPlugin.getTypeId());
+            if (svnProvider == null) return;
+
+            ISVNLocalResource svnResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+            if (svnResource == null) return;
+
+            LocalResourceStatus status = svnResource.getStatus();
+
+            if (status.getUrlCopiedFrom() != null) {
+
+                label = new Label(composite, SWT.NONE);
+                label.setText("URL Copied From");
+                urlCopiedFromValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+                urlCopiedFromValue.setText(status.getUrlCopiedFrom() != null ? status
+                        .getUrlCopiedFrom().toString() : "");
+            }
+
+            ignoredValue.setText(new Boolean(status.isIgnored()).toString());
+            managedValue.setText(new Boolean(status.isManaged()).toString());
+            hasRemoteValue.setText(new Boolean(status.isIgnored()).toString());
+            urlValue.setText(status.getUrl() != null ? status.getUrl().toString() : "");
+            lastChangedRevisionValue.setText(status.getLastChangedRevision() != null ? status
+                    .getLastChangedRevision().toString() : "");
+            lastChangedDateValue.setText(status.getLastChangedDate() != null ? status
+                    .getLastChangedDate().toString() : "");
+            lastCommitAuthorValue.setText(status.getLastCommitAuthor() != null ? status
+                    .getLastCommitAuthor() : "");
+            textStatusValue.setText(status.getTextStatus() != null ? status.getTextStatus()
+                    .toString() : "");
+            mergedValue.setText(new Boolean(status.isTextMerged()).toString());
+            deletedValue.setText(new Boolean(status.isDeleted()).toString());
+            modifiedValue.setText(new Boolean(status.isTextModified()).toString());
+            addedValue.setText(new Boolean(status.isAdded()).toString());
+            revisionValue.setText(status.getRevision() != null ? status.getRevision().toString()
+                    : "");
+            copiedValue.setText(new Boolean(status.isCopied()).toString());
+//            pathValue.setText(status.getPath() != null ? status.getPath() : "");
+
+        } catch (Exception e) {
+            SVNUIPlugin.log(new Status(Status.ERROR, SVNUIPlugin.ID, TeamException.UNABLE,
+                    "Property Exception", e));
+        }
+    }
+
+    /**
+     * @see PreferencePage#createContents(Composite)
+     */
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NONE);
+        GridLayout layout = new GridLayout();
+        composite.setLayout(layout);
+        GridData data = new GridData(GridData.FILL);
+
+        composite.setLayoutData(data);
+
+        addFirstSection(composite);
+        addSeparator(composite);
+        addSecondSection(composite);
+        
+        Dialog.applyDialogFont(parent);
+        return composite;
+    }
+
+    private Composite createDefaultComposite(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 2;
+        composite.setLayout(layout);
+
+        GridData data = new GridData();
+        data.verticalAlignment = GridData.FILL;
+        composite.setLayoutData(data);
+
+        return composite;
+    }
+
+    protected void performDefaults() {
+    }
+
+    public boolean performOk() {
+        return true;
+    }
+
+}
