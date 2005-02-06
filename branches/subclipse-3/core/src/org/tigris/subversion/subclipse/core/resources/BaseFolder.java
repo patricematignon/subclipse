74020d75201eb17a9c24057d5b9765572787d25b
brockj <brockj@a34601ff-79c0-0310-90bb-fac03367fbd1>
2005-02-06 00:05:35 +0000

    Fix for issue 165 - Compare with Base too slow
    Created a differencer that avoid content comparisions.  Changed implementation of BaseFolder to use "svn status" get get children.  There is still massive room for improvement since we have to do a recursive status update for each folder.  Unfortunately, we can't use the status cache since isn't possible to get the status of delted files.
    
    NOTE: External folders are not handled correctly yet.  They exist in the filesystem, but not in the base revisions.
    
    * RevisionAwareDifferencer.java
    Extracted from SVNCompareEditorInput
    try to avoid content comparisions for local files that are unchanged and have a last modified revision that matches the remote last modified revision
    
    * SVNCompareEditorInput.java
    Extracted RevisionAwareDifferencer
    
    * SVNLocalCompareInput.java
    Use an optimised differencer that compares local resources to base revisions.  This will only use the status to determine if a change has been made
    
    * BaseFolder.java
    (getMembers)Use "svn status" to get children.  "svn ls" will contact the server.
    
    Issue #: 165

--- branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/resources/BaseFolder.java
+++ branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/resources/BaseFolder.java
@@ -15,6 +15,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.tigris.subversion.subclipse.core.ISVNLocalResource;
@@ -23,12 +25,11 @@ import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
 import org.tigris.subversion.subclipse.core.Policy;
 import org.tigris.subversion.subclipse.core.SVNException;
 import org.tigris.subversion.subclipse.core.SVNStatus;
+import org.tigris.subversion.subclipse.core.commands.GetStatusCommand;
 import org.tigris.subversion.subclipse.core.util.Assert;
-import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
-import org.tigris.subversion.svnclientadapter.ISVNDirEntry;
-import org.tigris.subversion.svnclientadapter.SVNClientException;
 import org.tigris.subversion.svnclientadapter.SVNNodeKind;
 import org.tigris.subversion.svnclientadapter.SVNRevision;
+import org.tigris.subversion.svnclientadapter.SVNStatusKind;
 
 /**
  * represents the base revision of a folder
@@ -52,6 +53,7 @@ public class BaseFolder extends RemoteFolder implements ISVNRemoteFolder {
 	}
 	
 	/* (non-Javadoc)
+     * TODO This should use the synchronization information instead of hitting the WC
 	 * @see org.tigris.subversion.subclipse.core.resources.RemoteFolder#getMembers(org.eclipse.core.runtime.IProgressMonitor)
 	 */
 	protected ISVNRemoteResource[] getMembers(IProgressMonitor monitor)
@@ -69,44 +71,50 @@ public class BaseFolder extends RemoteFolder implements ISVNRemoteFolder {
         }
 		
 		try {
-            ISVNClientAdapter client = getRepository().getSVNClient();
-				
-			ISVNDirEntry[] list = client.getList(localResource.getFile(),getRevision(),false);
-			List result = new ArrayList();
+            List baseChildren = new ArrayList();
 
-			IContainer container = (IContainer)localResource.getIResource();			
-			// directories first				
-			for (int i=0;i<list.length;i++)
-			{
-                ISVNDirEntry entry = list[i];
-                if (entry.getNodeKind() == SVNNodeKind.DIR)
-				{
-				    result.add(new BaseFolder(
-	                   new LocalFolder(container.getFolder(new Path(entry.getPath()))), 
-                       entry.getLastChangedRevision(),
-                       entry.getLastChangedDate(),
-                       entry.getLastCommitAuthor()));
-				}
-			}
+            // XXX There is no way to get immediate children only
+            GetStatusCommand c = new GetStatusCommand(localResource);
+            c.run(monitor);
+            LocalResourceStatus[] statuses = c.getStatuses();
+            for (int i = 0; i < statuses.length; i++) {
+                if (localResource.getFile().equals(statuses[i].getFile())) {
+                    continue;
+                }
 
-			// files then				
-			for (int i=0;i<list.length;i++)
-			{
-				ISVNDirEntry entry = list[i];
-				if (entry.getNodeKind() == SVNNodeKind.FILE)
-				{
-					result.add(new BaseFile(
-                        new LocalFile(container.getFile(new Path(entry.getPath()))), 
-                        entry.getLastChangedRevision(),
-                        entry.getLastChangedDate(),
-                        entry.getLastCommitAuthor()));
-			     }
-					 	
-			}
+                // Don't create base entries for files that aren't managed yet
+                if (statuses[i].getTextStatus() == SVNStatusKind.UNVERSIONED) {
+                    continue;
+                }
+                
+                // External entries don't have base information either
+                if (statuses[i].getTextStatus() == SVNStatusKind.EXTERNAL) {
+                    continue;
+                }
+                
+                if (statuses[i].getNodeKind() == SVNNodeKind.DIR) {
+                    IContainer container = localResource.getIResource().getWorkspace().getRoot().getContainerForLocation(new Path(statuses[i].getFile().getAbsolutePath()));
+                    if (localResource.getIResource().equals(container.getParent())) {
+                        baseChildren.add(new BaseFolder(new LocalFolder(container), 
+                                statuses[i].getLastChangedRevision(),
+                                statuses[i].getLastChangedDate(),
+                                statuses[i].getLastCommitAuthor()));
+                    }
+                } else {
+                    IFile file = localResource.getIResource().getWorkspace().getRoot().getFileForLocation(new Path(statuses[i].getFile().getAbsolutePath()));
+                    if (localResource.getIResource().equals(file.getParent())) {
+                        baseChildren.add(new BaseFile(
+                                new LocalFile(file), 
+                                statuses[i].getLastChangedRevision(),
+                                statuses[i].getLastChangedDate(),
+                                statuses[i].getLastCommitAuthor()));
+                    }
+                }
+            }
 
-			children = (ISVNRemoteResource[])result.toArray(new ISVNRemoteResource[0]);
+            children = (ISVNRemoteResource[]) baseChildren.toArray(new ISVNRemoteResource[baseChildren.size()]);
             return children;
-        } catch (SVNClientException e)
+        } catch (CoreException e)
 		{
             throw new SVNException(new SVNStatus(SVNStatus.ERROR, SVNStatus.DOES_NOT_EXIST, Policy.bind("RemoteFolder.doesNotExist", getRepositoryRelativePath()))); //$NON-NLS-1$
         } finally {
