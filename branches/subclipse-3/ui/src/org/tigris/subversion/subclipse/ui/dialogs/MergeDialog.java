18fc5f5f3d43ca66cac0416f09c690f5cb198f54
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2005-02-16 15:09:27 +0000

    Enhancements to merge dialog.
    Our dialog was modelled on the dialog used in TortoiseSVN.  There were some discussions on
    their mailing lists about improving the UI of this dialog.  We are mirroring the changes that
    they made to their dialog in ours, and added some new options in the process.
    
    1)  Layout and terminology was tweaked to reinforce the idea that you a merge is taking
    the diff between 2 URL/revisions and applying that diff to your WC.
    
    2)  A new Show Log button was added that lets you browse the history of the URL and select
    revisions from that dialog.  The "select revision" process tries to figure out what kind of merge
    you might be doing and act accordingly to help you.
    
    a)  If you take Show Log from the WC button, and select a revision, it will apply that revision to
    the From revision field, provided that it is empty.  The idea here is that you are finding the revision
    for the last time you did a merge and want to use that as the starting point for this merge.  This is
    the "repeated" merge scenario of keeping a branch up to date with trunk or another branch.
    
    b)  If the "Use From URL" button is checked and you take the Show Log option from either the From
    or To revision fields, then the "revision range" you selected will be filled into the two fields.  The from
    revision will be the lowest revision number selected minus one (N -1) and the highest revision will be
    filled in the To revision.  So if you selected just revision 100, then 99 would be the From revision and
    100 would be the To revision.  This scenario is the "port" revisions from one branch to another
    scenario.
    
    c)  Finally, if the Use From URL button is not checked, then the revision you select is what is filled in
    to the field, and only that field.  
    
    In addition to these changes, buttons were added to a Dry run merge and also to produce a Unified 
    Diff file.  These two options allow you to "test the merge" settings.  As such, both options leave the
    dialog open so that you can then just press Merge with the same settings.
    
    The Dry run option currently only works for the Command Line adapter, but support for this option
    should make it into JavaHL in svn 1.2 timeframe.

--- branches/subclipse-3/ui/src/org/tigris/subversion/subclipse/ui/dialogs/MergeDialog.java
+++ branches/subclipse-3/ui/src/org/tigris/subversion/subclipse/ui/dialogs/MergeDialog.java
@@ -1,5 +1,6 @@
 package org.tigris.subversion.subclipse.ui.dialogs;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.text.ParseException;
 
@@ -8,6 +9,7 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -18,17 +20,23 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.help.WorkbenchHelp;
 import org.tigris.subversion.subclipse.core.ISVNLocalResource;
+import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
 import org.tigris.subversion.subclipse.core.SVNException;
+import org.tigris.subversion.subclipse.core.history.ILogEntry;
 import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
 import org.tigris.subversion.subclipse.ui.IHelpContextIds;
 import org.tigris.subversion.subclipse.ui.Policy;
 import org.tigris.subversion.subclipse.ui.util.UrlCombo;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
 import org.tigris.subversion.svnclientadapter.SVNRevision;
 import org.tigris.subversion.svnclientadapter.SVNUrl;
 
@@ -51,6 +59,8 @@ public class MergeDialog extends Dialog {
     private Button toRevisionButton;
     
     private Button okButton;
+    private Button diffButton;
+    private Button dryRunButton;
     
     private IResource resource;
     
@@ -58,6 +68,10 @@ public class MergeDialog extends Dialog {
     private SVNRevision fromRevision;
     private SVNUrl toUrl;
     private SVNRevision toRevision;
+    private ISVNLocalResource svnResource;
+    private File diffFile;
+    private File file;
+    private ISVNClientAdapter svnClient;
 
     public MergeDialog(Shell parentShell, IResource resource) {
         super(parentShell);
@@ -75,47 +89,42 @@ public class MergeDialog extends Dialog {
 		composite.setLayoutData(data);
 		
 		Label label = new Label(composite, SWT.NONE);
-		label.setText(Policy.bind("MergeDialog.url"));
+		label.setText(Policy.bind("MergeDialog.text")); //$NON-NLS-1$
 		
-		Text urlText = new Text(composite, SWT.BORDER);
-		data = new GridData();
-		data.widthHint = URL_WIDTH_HINT;
-		urlText.setLayoutData(data);
-		urlText.setEditable(false);
-		
-		ISVNLocalResource svnResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+		svnResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+		String urlString = null;
 		try {
             SVNUrl svnUrl = svnResource.getStatus().getUrl();
-            if (svnUrl != null) urlText.setText(svnResource.getStatus().getUrl().toString());
+            if (svnUrl != null) urlString = svnResource.getStatus().getUrl().toString();
         } catch (SVNException e1) {}
         
 		Group fromGroup = new Group(composite, SWT.NULL);
-		fromGroup.setText(Policy.bind("MergeDialog.from"));
+		fromGroup.setText(Policy.bind("MergeDialog.from")); //$NON-NLS-1$
 		GridLayout fromLayout = new GridLayout();
 		fromLayout.numColumns = 2;
 		fromGroup.setLayout(fromLayout);
 		
 		fromUrlCombo = new UrlCombo(fromGroup, resource.getProject().getName());
-		fromUrlCombo.setText(urlText.getText());
+		if (urlString != null) fromUrlCombo.setText(urlString);
 		
 		fromBrowseButton = new Button(fromGroup, SWT.PUSH);
-		fromBrowseButton.setText(Policy.bind("SwitchDialog.browse"));
+		fromBrowseButton.setText(Policy.bind("SwitchDialog.browse")); //$NON-NLS-1$
 		
 		Composite fromRevisionComposite = new Composite(fromGroup, SWT.NULL);
 		GridLayout fromRevisionLayout = new GridLayout();
-		fromRevisionLayout.numColumns = 2;
+		fromRevisionLayout.numColumns = 3;
 		fromRevisionComposite.setLayout(fromRevisionLayout);
 		data = new GridData(GridData.FILL_BOTH);
 		fromRevisionComposite.setLayoutData(data);
 		
 		fromHeadButton = new Button(fromRevisionComposite, SWT.RADIO);
-		fromHeadButton.setText(Policy.bind("SwitchDialog.head"));
+		fromHeadButton.setText(Policy.bind("SwitchDialog.head")); //$NON-NLS-1$
 		data = new GridData();
-		data.horizontalSpan = 2;
+		data.horizontalSpan = 3;
 		fromHeadButton.setLayoutData(data);
 		
 		fromRevisionButton = new Button(fromRevisionComposite, SWT.RADIO);
-		fromRevisionButton.setText(Policy.bind("SwitchDialog.revision"));
+		fromRevisionButton.setText(Policy.bind("SwitchDialog.revision")); //$NON-NLS-1$
 		
 		fromRevisionButton.setSelection(true);
 		
@@ -123,6 +132,14 @@ public class MergeDialog extends Dialog {
 		data = new GridData();
 		data.widthHint = REVISION_WIDTH_HINT;
 		fromRevisionText.setLayoutData(data);
+		
+		Button fromLogButton = new Button(fromRevisionComposite, SWT.PUSH);
+		fromLogButton.setText(Policy.bind("MergeDialog.showLog")); //$NON-NLS-1$
+		fromLogButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                showLog(fromRevisionText);
+            }
+		});
 		
 		SelectionListener fromListener = new SelectionAdapter() {
             public void widgetSelected(SelectionEvent e) {
@@ -139,7 +156,7 @@ public class MergeDialog extends Dialog {
 		fromRevisionButton.addSelectionListener(fromListener);		
 		
 		Group toGroup = new Group(composite, SWT.NULL);
-		toGroup.setText(Policy.bind("MergeDialog.to"));
+		toGroup.setText(Policy.bind("MergeDialog.to")); //$NON-NLS-1$
 		GridLayout toLayout = new GridLayout();
 		toLayout.numColumns = 2;
 		toGroup.setLayout(toLayout);
@@ -154,31 +171,31 @@ public class MergeDialog extends Dialog {
 		useFromUrlButton = new Button(useFromComposite, SWT.CHECK);
 		useFromUrlButton.setSelection(true);
 		
-		useFromUrlButton.setText(Policy.bind("MergeDialog.useFrom"));
+		useFromUrlButton.setText(Policy.bind("MergeDialog.useFrom")); //$NON-NLS-1$
 		
 		toUrlCombo = new UrlCombo(toGroup, resource.getProject().getName());
-		toUrlCombo.setText(urlText.getText());
+		toUrlCombo.setText(fromUrlCombo.getText());
 		toUrlCombo.getCombo().setVisible(false);
 		
 		toBrowseButton = new Button(toGroup, SWT.PUSH);
-		toBrowseButton.setText(Policy.bind("SwitchDialog.browse"));
-		toBrowseButton.setEnabled(false);
+		toBrowseButton.setText(Policy.bind("SwitchDialog.browse")); //$NON-NLS-1$
+		toBrowseButton.setVisible(false);
 		
 		Composite toRevisionComposite = new Composite(toGroup, SWT.NULL);
 		GridLayout toRevisionLayout = new GridLayout();
-		toRevisionLayout.numColumns = 2;
+		toRevisionLayout.numColumns = 3;
 		toRevisionComposite.setLayout(toRevisionLayout);
 		data = new GridData(GridData.FILL_BOTH);
 		toRevisionComposite.setLayoutData(data);
 		
 		toHeadButton = new Button(toRevisionComposite, SWT.RADIO);
-		toHeadButton.setText(Policy.bind("SwitchDialog.head"));
+		toHeadButton.setText(Policy.bind("SwitchDialog.head")); //$NON-NLS-1$
 		data = new GridData();
-		data.horizontalSpan = 2;
+		data.horizontalSpan = 3;
 		toHeadButton.setLayoutData(data);
 		
 		toRevisionButton = new Button(toRevisionComposite, SWT.RADIO);
-		toRevisionButton.setText(Policy.bind("SwitchDialog.revision"));
+		toRevisionButton.setText(Policy.bind("SwitchDialog.revision")); //$NON-NLS-1$
 		
 		toRevisionButton.setSelection(true);
 		
@@ -186,6 +203,14 @@ public class MergeDialog extends Dialog {
 		data = new GridData();
 		data.widthHint = REVISION_WIDTH_HINT;
 		toRevisionText.setLayoutData(data);
+		
+		Button toLogButton = new Button(toRevisionComposite, SWT.PUSH);
+		toLogButton.setText(Policy.bind("MergeDialog.showLog")); //$NON-NLS-1$
+		toLogButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                showLog(toRevisionText);
+            }
+		});
 		
 		SelectionListener toListener = new SelectionAdapter() {
             public void widgetSelected(SelectionEvent e) {
@@ -220,7 +245,7 @@ public class MergeDialog extends Dialog {
 		useFromUrlButton.addSelectionListener(new SelectionAdapter() {
             public void widgetSelected(SelectionEvent e) {
                 if (useFromUrlButton.getSelection()) toUrlCombo.setText(fromUrlCombo.getText());
-                toBrowseButton.setEnabled(!useFromUrlButton.getSelection());
+                toBrowseButton.setVisible(!useFromUrlButton.getSelection());
                 toUrlCombo.getCombo().setVisible(!useFromUrlButton.getSelection());
                 setOkButtonStatus();
             }		    
@@ -249,11 +274,182 @@ public class MergeDialog extends Dialog {
 		toUrlCombo.getCombo().addModifyListener(modifyListener);
 		toRevisionText.addModifyListener(modifyListener);
 		
+		Group workingGroup = new Group(composite, SWT.NULL);
+		GridLayout workingLayout = new GridLayout();
+		workingLayout.numColumns = 2;
+		workingGroup.setLayout(workingLayout);
+		data = new GridData(GridData.FILL_HORIZONTAL);
+		workingGroup.setLayoutData(data);
+		
+		Composite workingComposite = new Composite(workingGroup, SWT.NULL);
+		workingComposite.setLayout(new GridLayout());
+		data = new GridData(GridData.FILL_HORIZONTAL);
+		workingComposite.setLayoutData(data);		
+		
+		Label workingLabel = new Label(workingComposite, SWT.NONE);
+		workingLabel.setText(Policy.bind("MergeDialog.workingCopy")); //$NON-NLS-1$
+		
+		Text workingText = new Text(workingComposite, SWT.BORDER);
+		workingText.setEnabled(false);
+		data = new GridData();
+		data.widthHint = URL_WIDTH_HINT;
+		workingText.setLayoutData(data);
+		workingText.setText(resource.getWorkspace().getRoot().getLocation().toString());
+		
+		Label repositoryLabel = new Label(workingComposite, SWT.NONE);
+		repositoryLabel.setText(Policy.bind("MergeDialog.repositoryUrl")); //$NON-NLS-1$	
+		
+		Text repositoryText = new Text(workingComposite, SWT.BORDER);
+		repositoryText.setEnabled(false);
+		data = new GridData();
+		data.widthHint = URL_WIDTH_HINT;
+		repositoryText.setLayoutData(data);	
+		repositoryText.setText(urlString);
+		
+		Button workingLogButton = new Button(workingGroup, SWT.PUSH);
+		workingLogButton.setText(Policy.bind("MergeDialog.showLog")); //$NON-NLS-1$
+		workingLogButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                showLog(null);
+            }
+		});		
+		
 		fromUrlCombo.getCombo().setFocus();
 		WorkbenchHelp.setHelp(composite, IHelpContextIds.MERGE_DIALOG);
 
 		return composite;
 	}
+
+    protected void createButtonsForButtonBar(Composite parent) {
+        dryRunButton = createButton(parent, 2, Policy.bind("MergeDialog.dryRun"), false); //$NON-NLS-1$
+        dryRunButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                dryRun();
+            }
+        });
+        dryRunButton.setEnabled(false);
+        diffButton = createButton(parent, 3, Policy.bind("MergeDialog.diff"), false); //$NON-NLS-1$
+        diffButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                diff();
+            }
+        });   
+        diffButton.setEnabled(false);
+        super.createButtonsForButtonBar(parent);
+    }
+    
+    private void dryRun() {
+        try {
+            svnClient = svnResource.getRepository().getSVNClient();
+            file = resource.getLocation().toFile();
+            fromUrl = new SVNUrl(fromUrlCombo.getText());
+            toUrl = new SVNUrl(toUrlCombo.getText());
+            if (fromHeadButton.getSelection()) fromRevision = SVNRevision.HEAD;
+            else fromRevision = SVNRevision.getRevision(fromRevisionText.getText().trim());
+            if (toHeadButton.getSelection()) toRevision = SVNRevision.HEAD;
+            else toRevision = SVNRevision.getRevision(toRevisionText.getText().trim());                        
+            BusyIndicator.showWhile(Display.getCurrent(), new Runnable() {
+                public void run() {
+                    try {
+                        svnClient.merge(fromUrl, fromRevision, toUrl, toRevision, file, false, true, true);
+                    } catch (SVNClientException e) {
+                        MessageDialog.openError(getShell(), Policy.bind("MergeDialog.dryRun"), e.toString()); //$NON-NLS-1$
+                    }
+                }               
+            });       
+        } catch (Exception e) {
+            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.dryRun"), e.toString()); //$NON-NLS-1$            
+        }
+    }
+    
+    private void diff() {
+        FileDialog dialog = new FileDialog(getShell(), SWT.SAVE);
+        dialog.setText("Create Unified Diff File");
+        String returnFile = dialog.open();
+        if (returnFile == null) return;
+        diffFile = new File(returnFile);
+        try {
+            svnClient = svnResource.getRepository().getSVNClient();
+            fromUrl = new SVNUrl(fromUrlCombo.getText());
+            toUrl = new SVNUrl(toUrlCombo.getText());
+            if (fromHeadButton.getSelection()) fromRevision = SVNRevision.HEAD;
+            else fromRevision = SVNRevision.getRevision(fromRevisionText.getText().trim());
+            if (toHeadButton.getSelection()) toRevision = SVNRevision.HEAD;
+            else toRevision = SVNRevision.getRevision(toRevisionText.getText().trim());            
+            BusyIndicator.showWhile(Display.getCurrent(), new Runnable() {
+                public void run() {
+                    try {
+                        svnClient.diff(fromUrl, fromRevision, toUrl, toRevision, diffFile, true);
+                    } catch (SVNClientException e) {
+                        MessageDialog.openError(getShell(), Policy.bind("MergeDialog.diff"), e.toString()); //$NON-NLS-1$
+                    }
+                }               
+            });
+        } catch (Exception e) {
+            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.diff"), e.toString()); //$NON-NLS-1$
+        }
+    }
+    
+    private void showLog(Text text) {
+        ISVNRemoteResource remoteResource = null;
+        if (text == fromRevisionText) {
+	        try {
+	            fromUrl = new SVNUrl(fromUrlCombo.getText());
+	            remoteResource = svnResource.getRepository().getRemoteFile(fromUrl);
+	        } catch (Exception e) {
+	            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.showLog"), e.toString()); //$NON-NLS-1$
+	            return;
+	        }
+	        if (remoteResource == null) {
+	            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.showLog"), Policy.bind("MergeDialog.urlError") + " " + fromUrlCombo.getText()); //$NON-NLS-1$
+	            return;	            
+	        }
+        }
+        if (text == toRevisionText) {
+	        try {
+	            toUrl = new SVNUrl(toUrlCombo.getText());
+	            remoteResource = svnResource.getRepository().getRemoteFile(toUrl);
+	        } catch (Exception e) {
+	            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.showLog"), e.toString()); //$NON-NLS-1$
+	            return;
+	        }
+	        if (remoteResource == null) {
+	            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.showLog"), Policy.bind("MergeDialog.urlError") + " " + toUrlCombo.getText()); //$NON-NLS-1$
+	            return;	            
+	        }	        
+        }   
+        HistoryDialog dialog = null;
+        if ((text == fromRevisionText) || (text == toRevisionText))
+            dialog = new HistoryDialog(getShell(), remoteResource);
+        else
+            dialog = new HistoryDialog(getShell(), resource);
+        if (dialog.open() == HistoryDialog.CANCEL) return;
+        ILogEntry[] selectedEntries = dialog.getSelectedLogEntries();
+        if (selectedEntries.length == 0) return;
+        if ((text != null) && useFromUrlButton.getSelection()) {
+            fromRevisionText.setText(Long.toString(selectedEntries[selectedEntries.length - 1].getRevision().getNumber() - 1));
+            fromRevisionButton.setSelection(true);
+            fromHeadButton.setSelection(false);
+            toRevisionText.setText(Long.toString(selectedEntries[0].getRevision().getNumber()));
+            toRevisionButton.setSelection(true);
+            toHeadButton.setSelection(false);                  
+            fromRevisionText.setEnabled(true);
+            toRevisionText.setEnabled(true);
+            return;
+        }
+        if ((text == fromRevisionText) || ((text == null) && (fromRevisionText.getText().trim().length() == 0))) {
+            fromRevisionText.setText(Long.toString(selectedEntries[0].getRevision().getNumber()));
+            fromRevisionButton.setSelection(true);
+            fromHeadButton.setSelection(false); 
+            fromRevisionText.setEnabled(true);
+        }
+        if (text == toRevisionText) {
+            toRevisionText.setText(Long.toString(selectedEntries[0].getRevision().getNumber()));
+            toRevisionButton.setSelection(true);
+            toHeadButton.setSelection(false);
+            toRevisionText.setEnabled(true);
+        } 
+    }
 	
     protected void okPressed() {
         fromUrlCombo.saveUrl();
@@ -265,7 +461,7 @@ public class MergeDialog extends Dialog {
                 try {
                     fromRevision = SVNRevision.getRevision(fromRevisionText.getText().trim());
                 } catch (ParseException e1) {
-                  MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), Policy.bind("MergeDialog.invalidFrom"));
+                  MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), Policy.bind("MergeDialog.invalidFrom")); //$NON-NLS-1$
                   return;   
                 }
             }
@@ -276,12 +472,12 @@ public class MergeDialog extends Dialog {
                 try {
                     toRevision = SVNRevision.getRevision(toRevisionText.getText().trim());
                 } catch (ParseException e1) {
-                  MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), Policy.bind("MergeDialog.invalidTo"));
+                  MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), Policy.bind("MergeDialog.invalidTo")); //$NON-NLS-1$
                   return;   
                 }
             }            
         } catch (MalformedURLException e) {
-            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), e.getMessage());
+            MessageDialog.openError(getShell(), Policy.bind("MergeDialog.title"), e.getMessage()); //$NON-NLS-1$
             return;
         }
         super.okPressed();
@@ -291,6 +487,7 @@ public class MergeDialog extends Dialog {
         Button button = super.createButton(parent, id, label, defaultButton);
 		if (id == IDialogConstants.OK_ID) {
 		    okButton = button;
+		    okButton.setText(Policy.bind("MergeDialog.title")); //$NON-NLS-1$
 		    okButton.setEnabled(false);
 		}
         return button;
@@ -301,6 +498,8 @@ public class MergeDialog extends Dialog {
         if (!fromHeadButton.getSelection() && (fromRevisionText.getText().trim().length() == 0)) canFinish = false;
         else if (!toHeadButton.getSelection() && (toRevisionText.getText().trim().length() == 0)) canFinish = false;
         okButton.setEnabled(canFinish);
+        diffButton.setEnabled(canFinish);
+        dryRunButton.setEnabled(canFinish);
     }
 
     public SVNRevision getFromRevision() {
