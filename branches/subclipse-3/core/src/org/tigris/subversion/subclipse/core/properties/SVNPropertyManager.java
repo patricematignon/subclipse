59a98fa977889fa5e2b1f0c423006d04a6d3a29b
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2005-03-09 14:15:31 +0000

    Removed some unused imports

--- branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/properties/SVNPropertyManager.java
+++ branches/subclipse-3/core/src/org/tigris/subversion/subclipse/core/properties/SVNPropertyManager.java
@@ -2,8 +2,6 @@ package org.tigris.subversion.subclipse.core.properties;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
