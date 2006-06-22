be15c9bb4c28afa4e4f274f9f41eb571437d5c8f
letenay <letenay@a34601ff-79c0-0310-90bb-fac03367fbd1>
2006-06-22 22:22:25 +0000

    Tagging svnAnt 1.0.0

---
+++ tags/svnant/1.0.0/src/main/org/tigris/subversion/svnant/Info.java
@@ -0,0 +1,225 @@
+package org.tigris.subversion.svnant;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.ISVNInfo;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
+import org.tigris.subversion.svnclientadapter.SVNNodeKind;
+import org.tigris.subversion.svnclientadapter.SVNUrl;
+
+/**
+ * svn info
+ * @author Jeremy Whitlock
+ * <a href="mailto:jwhitlock@collab.net">jwhitlock@collab.net</a>
+ * @author Daniel Rall
+ */
+public class Info extends SvnCommand {
+
+    /**
+     * The target to retrieve properties for.
+     */
+    private String target = null;
+
+    /** Whether or not to print the properties. */
+    private boolean verbose = false;
+
+    /** String prepended to new property names. */
+    private String propPrefix = "svn.info.";
+
+    /** Client adapter info object. */
+    private ISVNInfo info = null;
+
+    /**
+     * Available directory properties.  Assumed to be a subset of
+     * {@link #FILE_PROP_NAMES}.
+     */
+    private static final String[] DIR_PROP_NAMES = {
+        "path", 
+        "url", 
+        "repouuid", 
+        "rev", 
+        "nodekind", 
+        "schedule", 
+        "author", 
+        "lastRev", 
+        "lastDate"
+    };
+
+    /** Available file properties. */
+    private static final String[] FILE_PROP_NAMES = {
+        "path", 
+        "name",
+        "url", 
+        "repouuid", 
+        "rev", 
+        "nodekind", 
+        "schedule", 
+        "author", 
+        "lastRev", 
+        "lastDate", 
+        "lastTextUpdate", 
+        "lastPropUpdate", 
+        "checksum"
+    };
+
+    /**
+     * @see org.tigris.subversion.svnant.SvnCommand#execute(org.tigris.subversion.svnclientadapter.ISVNClientAdapter)
+     */
+    public void execute(ISVNClientAdapter svnClient) throws BuildException {
+
+        Project project = getProject();
+        try {
+            this.info = acquireInfo(svnClient, this.target);
+            String[] propNames = (SVNNodeKind.DIR == this.info.getNodeKind() ?
+                                  DIR_PROP_NAMES : FILE_PROP_NAMES);
+            for (int i = 0; i < propNames.length; i++) {
+                String value = getValue(propNames[i]);
+                project.setProperty(propPrefix + propNames[i], value);
+                if (verbose) {
+                    logInfo(propPrefix + propNames[i] + ": " + value);
+                } else {
+                	logVerbose(propPrefix + propNames[i] + ": " + value);
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new BuildException("Failed to set 'info' properties", e);
+        }
+    }
+
+    /**
+     * Always contacts the repository.  In the future, might want to
+     * allow for use of
+     * <code>ISVNInfo.getInfoFromWorkingCopy()</code>, which uses only
+     * the meta data from the WC.
+     *
+     * @exception SVNClientException If ISVNInfo.getInfo(target)
+     * fails.
+     */
+    private ISVNInfo acquireInfo(ISVNClientAdapter svnClient, String target)
+        throws SVNClientException {
+        File targetAsFile = new File(Project.translatePath(target));
+        if (targetAsFile.exists()) {
+            // Since the target exists locally, assume it's not a URL.
+            return svnClient.getInfo(targetAsFile);
+        }
+        else {
+            try {
+                SVNUrl url = new SVNUrl(target);
+                return svnClient.getInfo(url);
+            }
+            catch (MalformedURLException malformedURL) {
+                // Since we don't have a valid URL with which to
+                // contact the repository, assume the target is a
+                // local file, even though it doesn't exist locally.
+                return svnClient.getInfo(targetAsFile);
+            }
+        }
+    }
+
+    /**
+     * Retrieve a value for the named property.  If the named property
+     * is not recognized and in verbose mode, log a message
+     * accordingly.  Assumes that {@link #info} has already been
+     * initialized (typically handled by invocation of {@link
+     * #execute()}).
+     *
+     * @param propName Name of the property to retrieve a value for.
+     * @return The value of the named property, or if not recognized,
+     * the empty string
+     */
+    public String getValue(String propName) {
+        Object value = null;
+
+        // ASSUMPTION: DIR_PROP_NAMES is a subset of FILE_PROP_NAMES.
+        if (FILE_PROP_NAMES[0].equals(propName)) {
+            value = this.info.getFile();
+            if (value != null) {
+                value = ((File) value).getAbsolutePath();
+            }
+        } else if (FILE_PROP_NAMES[1].equals(propName)) {
+            value = this.info.getFile();
+            if (value != null) {
+                value = ((File) value).getName();
+            }
+        } else if (FILE_PROP_NAMES[2].equals(propName)) {
+            value = this.info.getUrl();
+        } else if (FILE_PROP_NAMES[3].equals(propName)) {
+            value = this.info.getUuid();
+        } else if (FILE_PROP_NAMES[4].equals(propName)) {
+            value = this.info.getRevision();
+        } else if (FILE_PROP_NAMES[5].equals(propName)) {
+            value = this.info.getNodeKind();
+        } else if (FILE_PROP_NAMES[6].equals(propName)) {
+            value = this.info.getSchedule();
+        } else if (FILE_PROP_NAMES[7].equals(propName)) {
+            value = this.info.getLastCommitAuthor();
+        } else if (FILE_PROP_NAMES[8].equals(propName)) {
+            value = this.info.getLastChangedRevision();
+        } else if (FILE_PROP_NAMES[9].equals(propName)) {
+            value = this.info.getLastChangedDate();
+        } else if (FILE_PROP_NAMES[10].equals(propName)) {
+            value = this.info.getLastDateTextUpdate();
+        } else if (FILE_PROP_NAMES[11].equals(propName)) {
+            value = this.info.getLastDatePropsUpdate();
+        } else if (FILE_PROP_NAMES[12].equals(propName)) { 
+            // ### FIXME: Implement checksum in svnClientAdapter.
+            log("    " + "Property '" + propName + "' not implemented",
+                Project.MSG_WARN);
+        } else {
+            if (verbose) {
+                logInfo("    " + "Property '" + propName + "' not recognized");
+            } else {
+            	logVerbose("    " + "Property '" + propName + "' not recognized");
+            }
+        }
+
+        return (value == null ? "" : value.toString());
+    }
+
+    /**
+     * Validates the call to svn info
+     */
+    protected void validateAttributes() {
+        if (target == null) {
+            throw new BuildException("target must be set to a file or " +
+                                     "directory in your working copy, or " +
+                                     "to a URI");
+        }
+    }
+
+    /**
+     * Set the path to the target WC file or directory, or to an URI.
+     * @param target The target for which to retrieve info.
+     */
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    /**
+     * Sets whether or not we output the properties we set
+     * @param verbose
+     */
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    /**
+     * Sets the Ant property prefix.  The default is
+     * <code>svn.info.</code>.
+     *
+     * @param propPrefix The text to prefix all property names with.
+     */
+    public void setPropPrefix(String propPrefix) {
+        if (propPrefix.endsWith(".")) {
+            this.propPrefix = propPrefix;
+        } else {
+            this.propPrefix = propPrefix + '.';
+        }
+    }
+}
---
+++ tags/svnant/1.0.0/src/main/org/tigris/subversion/svnant/SvnCommand.java
@@ -0,0 +1,122 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */ 
+package org.tigris.subversion.svnant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectComponent;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.StringUtils;
+
+
+/**
+ * All ant svn commands inherits from this abstract class
+ * @author Cédric Chabanois 
+ *         <a href="mailto:cchabanois@ifrance.com">cchabanois@ifrance.com</a>
+ */
+public abstract class SvnCommand extends ProjectComponent {
+	protected SvnTask task;
+
+	protected abstract void validateAttributes();
+	
+	public abstract void execute(ISVNClientAdapter svnClient) throws BuildException;
+
+	public final void execute(SvnTask task, ISVNClientAdapter svnClient) throws BuildException
+	{
+		this.task = task;
+		String[] nameSegments = StringUtils.split(getClass().getName(), ".");
+		String className = "<" + nameSegments[nameSegments.length -1] + ">"; 
+			
+		logInfo(className + " started ...");
+		try {
+			execute(svnClient);
+		} catch (BuildException ex) {
+			logInfo(className + " failed !");
+			throw ex;
+		}
+		logInfo(className + " finished.");
+	}
+
+	public void logVerbose(String message)
+	{
+		getProject().log(this.task, message, Project.MSG_VERBOSE);
+	}
+
+	public void logWarning(String message)
+	{
+		getProject().log(this.task, message, Project.MSG_WARN);
+	}
+
+	public void logError(String message)
+	{
+		getProject().log(this.task, message, Project.MSG_ERR);
+	}
+
+	public void logInfo(String message)
+	{
+		getProject().log(this.task, message, Project.MSG_INFO);
+	}
+
+	public void log(String message)
+	{
+		getProject().log(this.task, message, Project.MSG_INFO);
+	}
+
+	public void log(String message, int level)
+	{
+		getProject().log(this.task, message, level);
+	}
+
+}
