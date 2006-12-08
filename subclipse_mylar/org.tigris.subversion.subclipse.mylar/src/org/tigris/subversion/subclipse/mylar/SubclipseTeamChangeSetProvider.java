a36c554d3ea505a6c84e5c7f5a7a73b9b0fb46ef
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2006-12-08 16:11:34 +0000

    Think this gets change sets working.
    The main change is probably the way the extension is declared in the plugin.xml.
    I added in the second element that Mylar has for the CVS extension.
    
    I also added a new class and moved code to it for the CommitWorkflow.  As near
    as I can tell, this code will not be used anymore as the Commit Context action
    was removed from Mylar.  Just making things match what the CVS integration does.
    
    It does seem to work now though.

--- subclipse_mylar/org.tigris.subversion.subclipse.mylar/src/org/tigris/subversion/subclipse/mylar/SubclipseTeamChangeSetProvider.java
+++ subclipse_mylar/org.tigris.subversion.subclipse.mylar/src/org/tigris/subversion/subclipse/mylar/SubclipseTeamChangeSetProvider.java
@@ -11,42 +11,19 @@
 
 package org.tigris.subversion.subclipse.mylar;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.eclipse.core.resources.IResource;
 import org.eclipse.mylar.team.AbstractActiveChangeSetProvider;
 import org.eclipse.team.internal.core.subscribers.ActiveChangeSetManager;
 import org.tigris.subversion.subclipse.core.SVNProviderPlugin;
-import org.tigris.subversion.subclipse.ui.actions.CommitAction;
 
 /**
  * Team repository provider for Mylar.
  * 
  * @author Eugene Kuleshov
  */
-public class SubclipseTeamChangeSetProvider extends AbstractActiveChangeSetProvider {
+public class SubclipseTeamChangeSetProvider extends
+		AbstractActiveChangeSetProvider {
 
 	public ActiveChangeSetManager getActiveChangeSetManager() {
-		// collectors.add((CVSActiveChangeSetCollector)CVSUIPlugin.getPlugin().getChangeSetManager());
-    return SVNProviderPlugin.getPlugin().getChangeSetManager();
-	}
-
-	public boolean hasOutgoingChanges(IResource[] resources) {
-	  CommitAction commitAction = new CommitAction("");
-    commitAction.setSelectedResources(resources);
-    return commitAction.hasOutgoingChanges();
-	}
-
-	public void commit(IResource[] resources) {
-    CommitAction commitAction = new CommitAction("");
-    commitAction.setSelectedResources(resources);
-    try {
-      commitAction.execute(null);
-    } catch (InvocationTargetException ex) {
-      // ignore
-    } catch (InterruptedException ex) {
-      // ignore
-    }
+		return SVNProviderPlugin.getPlugin().getChangeSetManager();
 	}
 }
-
