c403cde47f017dda35482bb5419597f7d38f756c
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2008-02-01 01:06:31 +0000

    Tag for 1.3.9 release

---
+++ 1.3.9/svnClientAdapter/src/main/org/tigris/subversion/svnclientadapter/ISVNConflictResolver.java
@@ -0,0 +1,46 @@
+package org.tigris.subversion.svnclientadapter;
+
+public interface ISVNConflictResolver {
+    /**
+     * The callback method invoked for each conflict during a
+     * merge/update/switch operation.
+     *
+     * @param descrip A description of the conflict.
+     * @return The result of any conflict resolution.
+     * @throws SubversionException If an error occurs.
+     */
+    public SVNConflictResult resolve(SVNConflictDescriptor descrip) throws SVNClientException;
+
+    /**
+     * From JavaHL
+     */
+    public final class Choice
+    {
+        /**
+         * User did nothing; conflict remains.
+         */
+        public static final int postpone = 0;
+
+        /**
+         * User chooses the base file.
+         */
+        public static final int chooseBase = 1;
+
+        /**
+         * User chooses the repository file.
+         */
+        public static final int chooseTheirs = 2;
+
+        /**
+         * User chooses own version of file.
+         */
+        public static final int chooseMine = 3;
+
+        /**
+         * User chooses the merged-file (which she may have manually
+         * edited).
+         */
+        public static final int chooseMerged = 4;
+    }
+
+}
