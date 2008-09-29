d247f8a6d71ff8fc422dfb4de65e819db7006c90
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2008-09-29 13:36:27 +0000

    Create tag for 1.4.5

---
+++ 1.4.5/org.tigris.subversion.subclipse.core/src/org/tigris/subversion/subclipse/core/commands/SwitchToUrlCommand.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2006 Subclipse project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Subclipse project committers - initial API and implementation
+ ******************************************************************************/
+package org.tigris.subversion.subclipse.core.commands;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.tigris.subversion.subclipse.core.ISVNCoreConstants;
+import org.tigris.subversion.subclipse.core.Policy;
+import org.tigris.subversion.subclipse.core.SVNException;
+import org.tigris.subversion.subclipse.core.client.OperationManager;
+import org.tigris.subversion.subclipse.core.client.OperationProgressNotifyListener;
+import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
+import org.tigris.subversion.svnclientadapter.SVNRevision;
+import org.tigris.subversion.svnclientadapter.SVNUrl;
+
+/**
+ * Switch URL for selected resource
+ */
+public class SwitchToUrlCommand implements ISVNCommand {
+	// resource to switch
+    private IResource resource;  
+    
+    private SVNUrl svnUrl;
+    
+    private SVNRevision svnRevision;
+    
+    private SVNWorkspaceRoot root;
+    
+    private int depth = ISVNCoreConstants.DEPTH_UNKNOWN;
+    private boolean setDepth = false;
+    private boolean ignoreExternals = false;
+    private boolean force = true;
+
+    public SwitchToUrlCommand(SVNWorkspaceRoot root, IResource resource, SVNUrl svnUrl, SVNRevision svnRevision) {
+        super();
+        this.root = root;
+        this.resource = resource;
+        this.svnUrl = svnUrl;
+        this.svnRevision = svnRevision;
+    }
+
+	/* (non-Javadoc)
+	 * @see org.tigris.subversion.subclipse.core.commands.ISVNCommand#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void run(IProgressMonitor monitor) throws SVNException {       
+		final IProgressMonitor subPm = Policy.infiniteSubMonitorFor(monitor, 100);
+        try {
+    		subPm.beginTask(null, Policy.INFINITE_PM_GUESS_FOR_SWITCH);
+            ISVNClientAdapter svnClient = root.getRepository().getSVNClient();
+            OperationManager.getInstance().beginOperation(svnClient, new OperationProgressNotifyListener(subPm));
+            File file = resource.getLocation().toFile();
+            SVNRevision pegRevision = null;
+//            ISVNLocalResource svnResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+//            if (svnResource != null && svnResource.getRevision() != null) pegRevision = svnResource.getRevision();
+//            else pegRevision = svnRevision;
+            pegRevision = SVNRevision.HEAD;
+            svnClient.switchToUrl(file, svnUrl, svnRevision, pegRevision, depth, setDepth, ignoreExternals, force);
+            try {
+                // Refresh the resource after merge
+                resource.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+            } catch (CoreException e1) {
+            }
+        } catch (SVNClientException e) {
+            throw SVNException.wrapException(e);
+        } finally {
+            OperationManager.getInstance().endOperation();
+            subPm.done();
+        }
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	
+	public void setSetDepth(boolean setDepth) {
+		this.setDepth = setDepth;
+	}
+
+	public void setIgnoreExternals(boolean ignoreExternals) {
+		this.ignoreExternals = ignoreExternals;
+	}
+
+	public void setForce(boolean force) {
+		this.force = force;
+	}
+
+}
