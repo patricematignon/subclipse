a3b78048830d7ad72d348092e9ed4a0a84f741c9
selsemore <selsemore@a34601ff-79c0-0310-90bb-fac03367fbd1>
2008-10-01 22:55:45 +0000

    Retrieve fonts from Eclipse preferences rather than creating fonts (the fonts that we were created were not being disposed either).

--- branches/GSoC2008/gimenete/org.tigris.subversion.subclipse.graph/src/org/tigris/subversion/subclipse/graph/editors/NodeTooltipFigure.java
+++ branches/GSoC2008/gimenete/org.tigris.subversion.subclipse.graph/src/org/tigris/subversion/subclipse/graph/editors/NodeTooltipFigure.java
@@ -9,7 +9,7 @@ import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.ToolbarLayout;
 import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.swt.SWT;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.tigris.subversion.sublicpse.graph.cache.Node;
@@ -23,10 +23,6 @@ public class NodeTooltipFigure extends Figure {
 	private static final int BORDER_WIDTH2 = BORDER_WIDTH*2;
 	
 	private static DateFormat dateFormat;
-	private static Font info = null;
-	private static Font authorFont = null;
-	private static Font dateFont = null;
-	private static Font plain = null;
 
 	private boolean hasSources = false;
 	private boolean hasTags = false;
@@ -42,26 +38,22 @@ public class NodeTooltipFigure extends Figure {
 		// lazy loading and reuse
 		if(dateFormat == null) {
 			dateFormat = SimpleDateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
-			info = new Font(null, "Arial", 12, SWT.BOLD);
-			authorFont = new Font(null, "Arial", 10, SWT.BOLD);
-			dateFont = new Font(null, "Arial", 10, SWT.ITALIC);
-			plain = new Font(null, "Arial", 10, SWT.NONE);
 		}
 
 		Figure tooltip = new Figure();
 		setToolTip(tooltip);
 
-		add(createLabel("Action and path", info, FONT_COLOR));
-		add(createLabel(node.getAction()+" "+node.getPath(), plain));
-		add(createLabel("Author", info, FONT_COLOR));
-		add(createLabel(node.getAuthor(), authorFont));
-		add(createLabel("Date", info, FONT_COLOR));
-		add(createLabel(dateFormat.format(node.getRevisionDate()), dateFont));
-		add(createLabel("Message", info, FONT_COLOR));
-		add(createLabel(node.getMessage(), dateFont));
+		add(createLabel("Action and path", JFaceResources.getHeaderFont(), FONT_COLOR));
+		add(createLabel(node.getAction()+" "+node.getPath(), JFaceResources.getTextFont()));
+		add(createLabel("Author", JFaceResources.getHeaderFont(), FONT_COLOR));
+		add(createLabel(node.getAuthor(), JFaceResources.getTextFont()));
+		add(createLabel("Date", JFaceResources.getHeaderFont(), FONT_COLOR));
+		add(createLabel(dateFormat.format(node.getRevisionDate()), JFaceResources.getTextFont()));
+		add(createLabel("Message", JFaceResources.getHeaderFont(), FONT_COLOR));
+		add(createLabel(node.getMessage(), JFaceResources.getTextFont()));
 		if(node.getCopySrcPath() != null) {
-			add(createLabel("From", info, FONT_COLOR));
-			add(createLabel(format(node.getCopySrcRevision(), node.getCopySrcPath()), plain));
+			add(createLabel("From", JFaceResources.getHeaderFont(), FONT_COLOR));
+			add(createLabel(format(node.getCopySrcRevision(), node.getCopySrcPath()), JFaceResources.getTextFont()));
 		}
 	}
 	
@@ -74,18 +66,18 @@ public class NodeTooltipFigure extends Figure {
 	
 	public void addSource(Node node) {
 		if(!hasSources) {
-			add(createLabel("Source of", info, FONT_COLOR));
+			add(createLabel("Source of", JFaceResources.getHeaderFont(), FONT_COLOR));
 			hasSources = true;
 		}
-		add(createLabel(format(node.getRevision(), node.getPath()), plain));
+		add(createLabel(format(node.getRevision(), node.getPath()), JFaceResources.getTextFont()));
 	}
 	
 	public void addTag(Node node) {
 		if(!hasTags) {
-			add(createLabel("Tagged as", info, FONT_COLOR));
+			add(createLabel("Tagged as", JFaceResources.getHeaderFont(), FONT_COLOR));
 			hasTags = true;
 		}
-		add(createLabel(format(node.getRevision(), node.getPath()), plain));
+		add(createLabel(format(node.getRevision(), node.getPath()), JFaceResources.getTextFont()));
 	}
 	
 	public String format(long revision, String path) {
