f87d004ad52624827db9b2ab8dcf91747d137007
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2008-12-05 18:06:59 +0000

    Create tag for 1.4.7 release

---
+++ 1.4.7/subclipse/org.tigris.subversion.subclipse.graph/src/org/tigris/subversion/subclipse/graph/cache/Node.java
@@ -0,0 +1,272 @@
+package org.tigris.subversion.subclipse.graph.cache;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.eclipse.ui.views.properties.PropertyDescriptor;
+
+public class Node implements Serializable, IPropertySource {
+	
+	private static final long serialVersionUID = 2835522933811459843L;
+	
+	// fields read from log messages
+	private long revision;
+	private String author;
+	private Date revisionDate;
+	private String message;
+	private String path;
+	private char action;
+	private long copySrcRevision;
+	private String copySrcPath;
+	
+	// other fields
+	private Node parent;
+	private Node source;
+	private int childCount;
+	private transient Object view;
+	
+	private List mergedRevisions;
+	private List tags;
+	
+	private transient Branch branch;
+	private transient int index;
+	
+	private static DateFormat dateFormat;
+
+	public static String P_ID_ACTION = "action";
+	public static String P_ACTION = "Action";
+	public static String P_ID_PATH = "path";
+	public static String P_PATH = "Path";	
+	public static String P_ID_DATE = "date";
+	public static String P_DATE = "Date";	
+	public static String P_ID_AUTHOR = "author";
+	public static String P_AUTHOR = "Author";
+	public static String P_ID_FROM = "from";
+	public static String P_FROM = "From";	
+	public static String P_ID_MSG = "msg";
+	public static String P_MSG = "Message";	
+	public static List descriptors;
+	static
+	{	
+		descriptors = new ArrayList();
+		descriptors.add(new PropertyDescriptor(P_ID_ACTION, P_ACTION));
+		descriptors.add(new PropertyDescriptor(P_ID_PATH, P_PATH));
+		descriptors.add(new PropertyDescriptor(P_ID_DATE, P_DATE));
+		descriptors.add(new PropertyDescriptor(P_ID_AUTHOR, P_AUTHOR));
+		descriptors.add(new PropertyDescriptor(P_ID_MSG, P_MSG));
+		descriptors.add(new PropertyDescriptor(P_ID_FROM, P_FROM));
+	}		
+
+	public Node() {
+	}
+	
+	public Node getParent() {
+		return parent;
+	}
+
+	public void setParent(Node parent) {
+		this.parent = parent;
+		if(parent != null)
+			parent.childCount++;
+	}
+
+	public long getRevision() {
+		return revision;
+	}
+
+	public void setRevision(long revision) {
+		this.revision = revision;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getRevisionDate() {
+		return revisionDate;
+	}
+
+	public void setRevisionDate(Date revisionDate) {
+		this.revisionDate = revisionDate;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public char getAction() {
+		return action;
+	}
+
+	public void setAction(char action) {
+		this.action = action;
+	}
+
+	public long getCopySrcRevision() {
+		return copySrcRevision;
+	}
+
+	public void setCopySrcRevision(long copySrcRevision) {
+		this.copySrcRevision = copySrcRevision;
+	}
+
+	public String getCopySrcPath() {
+		return copySrcPath;
+	}
+
+	public void setCopySrcPath(String copySrcPath) {
+		this.copySrcPath = copySrcPath;
+	}
+	
+	public String toString() {
+		String pattern = "{0} by {1} {3} on {2} -- {4} --";
+		return MessageFormat.format(pattern, 
+				new Object[]{ new Long(revision), 
+				author, path, action+"", message});
+	}
+
+	public Object getView() {
+		return view;
+	}
+
+	public void setView(Object view) {
+		this.view = view;
+	}
+	
+	public int getChildCount() {
+		return childCount;
+	}
+
+	public Node getSource() {
+		return source;
+	}
+
+	public void setSource(Node source) {
+		this.source = source;
+		if(source != null)
+			source.childCount++;
+	}
+	
+	public List getMergedRevisions() {
+		return mergedRevisions;
+	}
+	
+	public void addMergedRevision(Node node) {
+		if(mergedRevisions == null) {
+			mergedRevisions = new ArrayList();
+		}
+		mergedRevisions.add(node);
+	}
+	
+	public List getTags() {
+		return tags;
+	}
+	
+	public void addTag(Node node) {
+		if(tags == null) {
+			tags = new ArrayList();
+		}
+		tags.add(node);
+	}
+	
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+	
+	public Branch getBranch() {
+		return branch;
+	}
+	
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public Object getEditableValue() {
+		return "r" + revision;
+	}
+
+	public IPropertyDescriptor[] getPropertyDescriptors() {
+		return (IPropertyDescriptor[])getDescriptors().toArray(new IPropertyDescriptor[getDescriptors().size()]);
+	}
+	
+	private static List getDescriptors() {
+		return descriptors;
+	}	
+
+	public Object getPropertyValue(Object propKey) {
+		if (P_ID_MSG.equals(propKey)) {
+			if (message != null) {
+				return message;
+			}
+		}
+		if (P_ID_AUTHOR.equals(propKey)) {
+			if (author != null) {
+				return author;
+			}
+		}
+		if (P_ID_ACTION.equals(propKey)) {
+			return action + "";
+		}
+		if (P_ID_PATH.equals(propKey)) {
+			if (path != null) {
+				return path;
+			}
+		}
+		if (P_ID_FROM.equals(propKey)) {
+			if (copySrcPath != null) {
+				return "r"+Long.toString(copySrcRevision)+" "+copySrcPath;
+			}
+		}
+		if (P_ID_DATE.equals(propKey)) {
+			if (revisionDate != null) {
+				return getDateFormat().format(revisionDate);
+			}
+		}
+		return "";
+	}
+
+	public boolean isPropertySet(Object arg0) {
+		return false;
+	}
+
+	public void resetPropertyValue(Object arg0) {		
+	}
+
+	public void setPropertyValue(Object arg0, Object arg1) {
+	}
+	
+	private static DateFormat getDateFormat() {
+		if(dateFormat == null) {
+			dateFormat = SimpleDateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+		}
+		return dateFormat;
+	}	
+	
+}
---
+++ 1.4.7/subclipse/org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/properties/SVNPropertyPage.java
@@ -0,0 +1,296 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2006 Subclipse project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Subclipse project committers - initial API and implementation
+ ******************************************************************************/
+package org.tigris.subversion.subclipse.ui.properties;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.tigris.subversion.subclipse.core.ISVNLocalResource;
+import org.tigris.subversion.subclipse.core.SVNProviderPlugin;
+import org.tigris.subversion.subclipse.core.SVNTeamProvider;
+import org.tigris.subversion.subclipse.core.resources.LocalResourceStatus;
+import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+import org.tigris.subversion.subclipse.ui.IHelpContextIds;
+import org.tigris.subversion.subclipse.ui.Policy;
+import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.ISVNInfo;
+import org.tigris.subversion.svnclientadapter.ISVNProperty;
+import org.tigris.subversion.svnclientadapter.SVNRevision;
+
+public class SVNPropertyPage extends PropertyPage {
+
+    private Text ignoredValue;
+    private Text managedValue;
+    private Text switchedValue;
+    private Text urlValue;
+    private Text lastChangedRevisionValue;
+    private Text lastChangedDateValue;
+    private Text lastCommitAuthorValue;
+    private Text textStatusValue;
+    private Text mergedValue;
+    private Text deletedValue;
+    private Text modifiedValue;
+    private Text addedValue;
+    private Text revisionValue;
+    private Text copiedValue;
+    private Label urlCopiedFromValue;
+    private Text lockOwner;
+    private Text lockCreationDate;
+    private Label lockComment;
+   
+
+    public SVNPropertyPage() {
+        super();
+    }
+
+    private void addFirstSection(Composite parent) {
+        Composite composite = createDefaultComposite(parent);
+
+        //Label for path field
+        Label label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.path")); //$NON-NLS-1$
+
+        // Path text field
+        Text pathValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+        GridData gd = new GridData();
+        gd.widthHint = 500;
+        pathValue.setLayoutData(gd);
+        pathValue.setText(((IResource) getElement()).getFullPath().toString());
+        pathValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.url")); //$NON-NLS-1$
+        
+        urlValue = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+        gd = new GridData();
+        gd.widthHint = 500;
+        urlValue.setLayoutData(gd);
+        urlValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.revision")); //$NON-NLS-1$
+        revisionValue = new Text(composite, SWT.READ_ONLY);
+        revisionValue.setBackground(composite.getBackground());
+    }
+
+    private void addSeparator(Composite parent) {
+        Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+        GridData gridData = new GridData();
+        gridData.horizontalAlignment = GridData.FILL;
+        gridData.grabExcessHorizontalSpace = true;
+        separator.setLayoutData(gridData);
+    }
+
+    private void addSecondSection(Composite parent) {
+        Composite composite = createDefaultComposite(parent);
+
+        Label label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.ignored")); //$NON-NLS-1$
+        ignoredValue = new Text(composite, SWT.READ_ONLY);
+        ignoredValue.setBackground(composite.getBackground());
+        ignoredValue.setLayoutData(new GridData());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.managed")); //$NON-NLS-1$
+        managedValue = new Text(composite, SWT.READ_ONLY);
+        managedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.switched")); //$NON-NLS-1$
+        switchedValue = new Text(composite, SWT.READ_ONLY);
+        switchedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.changedRevision")); //$NON-NLS-1$
+        lastChangedRevisionValue = new Text(composite, SWT.READ_ONLY);
+        lastChangedRevisionValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.changedDate")); //$NON-NLS-1$
+        lastChangedDateValue = new Text(composite, SWT.READ_ONLY);
+        lastChangedDateValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.changedAuthor")); //$NON-NLS-1$
+        lastCommitAuthorValue = new Text(composite, SWT.READ_ONLY);
+        lastCommitAuthorValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.status")); //$NON-NLS-1$
+        textStatusValue = new Text(composite, SWT.READ_ONLY);
+        textStatusValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.merged")); //$NON-NLS-1$
+        mergedValue = new Text(composite, SWT.READ_ONLY);
+        mergedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.deleted")); //$NON-NLS-1$
+        deletedValue = new Text(composite, SWT.READ_ONLY);
+        deletedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.modified")); //$NON-NLS-1$
+        modifiedValue = new Text(composite, SWT.READ_ONLY);
+        modifiedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.added")); //$NON-NLS-1$
+        addedValue = new Text(composite, SWT.READ_ONLY);
+        addedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.copied")); //$NON-NLS-1$
+        copiedValue = new Text(composite, SWT.READ_ONLY);
+        copiedValue.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.lockOwner"));  //$NON-NLS-1$
+        lockOwner = new Text(composite, SWT.READ_ONLY);
+        lockOwner.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.lockCreationDate"));  //$NON-NLS-1$
+        lockCreationDate = new Text(composite, SWT.READ_ONLY);
+        lockCreationDate.setBackground(composite.getBackground());
+
+        label = new Label(composite, SWT.NONE);
+        label.setText(Policy.bind("SVNPropertyPage.lockComment"));  //$NON-NLS-1$
+        lockComment = new Label(composite, SWT.WRAP);
+        GridData gd = new GridData();
+        gd.widthHint = 500;
+        lockComment.setLayoutData(gd);
+
+        // Populate owner text field
+        try {
+            IResource resource = (IResource) getElement();
+            SVNTeamProvider svnProvider = (SVNTeamProvider) RepositoryProvider.getProvider(resource
+                    .getProject(), SVNProviderPlugin.getTypeId());
+            if (svnProvider == null) return;
+
+            ISVNLocalResource svnResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+            if (svnResource == null) return;
+
+            LocalResourceStatus status = svnResource.getStatus();
+            SVNRevision revision = svnResource.getRevision();
+
+            if (status.getUrlCopiedFrom() != null) {
+                label = new Label(composite, SWT.NONE);
+                label.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+                label.setText(Policy.bind("SVNPropertyPage.copiedFrom")); //$NON-NLS-1$
+                urlCopiedFromValue = new Label(composite, SWT.WRAP);
+                gd = new GridData();
+                gd.widthHint = 500;
+                urlCopiedFromValue.setLayoutData(gd);
+                urlCopiedFromValue.setText(status.getUrlCopiedFrom() != null ? status
+                        .getUrlCopiedFrom().toString() : ""); //$NON-NLS-1$
+            }
+
+            ignoredValue.setText(new Boolean(status.isIgnored()).toString());
+            managedValue.setText(new Boolean(status.isManaged()).toString());
+            switchedValue.setText(new Boolean(status.isSwitched()).toString());
+            urlValue.setText(status.getUrlString() != null ? status.getUrlString() : ""); //$NON-NLS-1$
+            lastChangedRevisionValue.setText(status.getLastChangedRevision() != null ? status
+                    .getLastChangedRevision().toString() : ""); //$NON-NLS-1$
+            lastChangedDateValue.setText(status.getLastChangedDate() != null ? status
+                    .getLastChangedDate().toString() : ""); //$NON-NLS-1$
+            lastCommitAuthorValue.setText(status.getLastCommitAuthor() != null ? status
+                    .getLastCommitAuthor() : ""); //$NON-NLS-1$
+            textStatusValue.setText(status.getTextStatus() != null ? status.getTextStatus()
+                    .toString() : ""); //$NON-NLS-1$
+            mergedValue.setText(new Boolean(status.isTextMerged()).toString());
+            deletedValue.setText(new Boolean(status.isDeleted()).toString());
+            modifiedValue.setText(new Boolean(status.isTextModified()).toString());
+            addedValue.setText(new Boolean(status.isAdded()).toString());
+            revisionValue.setText(revision != null ? revision.toString() : ""); //$NON-NLS-1$
+            copiedValue.setText(new Boolean(status.isCopied()).toString());
+            lockOwner.setText(status.getLockOwner() != null ? status.getLockOwner() : ""); //$NON-NLS-1$
+            lockCreationDate.setText(status.getLockOwner() != null ? status
+                    .getLockCreationDate().toString() : ""); //$NON-NLS-1$
+            lockComment.setText(status.getLockOwner() != null ? status.getLockComment() : ""); //$NON-NLS-1$
+            // Get lock information from server if svn:needs-lock property is set
+            if (status.getLockOwner() == null && status.getUrlString() != null) {
+           		ISVNProperty prop = svnResource.getSvnProperty("svn:needs-lock");
+           		if (prop != null) {
+	           	    ISVNClientAdapter client = svnResource.getRepository().getSVNClient();
+	            	try {
+	            		ISVNInfo info = client.getInfo(status.getUrl());
+	                    lockOwner.setText(info.getLockOwner() != null ? info.getLockOwner() : ""); //$NON-NLS-1$
+	                    lockCreationDate.setText(info.getLockOwner() != null ? info
+	                            .getLockCreationDate().toString() : ""); //$NON-NLS-1$
+	                    lockComment.setText(info.getLockOwner() != null ? info.getLockComment() : ""); //$NON-NLS-1$
+	            	} catch (Exception e) {
+	            	}
+           		}
+            }
+        } catch (Exception e) {
+            SVNUIPlugin.log(new Status(IStatus.ERROR, SVNUIPlugin.ID, TeamException.UNABLE,
+                    "Property Exception", e)); //$NON-NLS-1$
+        }
+    }
+
+    /**
+     * @see PreferencePage#createContents(Composite)
+     */
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NONE);
+        GridLayout layout = new GridLayout();
+        composite.setLayout(layout);
+        GridData data = new GridData(GridData.FILL);
+
+        composite.setLayoutData(data);
+
+        addFirstSection(composite);
+        addSeparator(composite);
+        addSecondSection(composite);
+        
+        Dialog.applyDialogFont(parent);
+        
+        PlatformUI.getWorkbench().getHelpSystem().setHelp(composite, IHelpContextIds.SVN_RESOURCE_PROPERTIES_PAGE);
+
+        return composite;
+    }
+
+    private Composite createDefaultComposite(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 2;
+        composite.setLayout(layout);
+
+        GridData data = new GridData(GridData.FILL_BOTH);
+//        data.verticalAlignment = GridData.FILL;
+        composite.setLayoutData(data);
+
+        return composite;
+    }
+
+    protected void performDefaults() {
+    }
+
+    public boolean performOk() {
+        return true;
+    }
+
+}
