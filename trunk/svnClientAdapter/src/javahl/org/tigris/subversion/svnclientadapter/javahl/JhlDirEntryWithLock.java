2d31a3a1cab36823f84d7dc3e22055bc17f01fee
markphip <markphip@a34601ff-79c0-0310-90bb-fac03367fbd1>
2011-04-19 19:44:23 +0000

    Reintegrate the svn-1.7 branch to trunk

--- trunk/svnClientAdapter/src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlDirEntryWithLock.java
+++ trunk/svnClientAdapter/src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlDirEntryWithLock.java
@@ -1,7 +1,7 @@
 package org.tigris.subversion.svnclientadapter.javahl;
 
-import org.tigris.subversion.javahl.DirEntry;
-import org.tigris.subversion.javahl.Lock;
+import org.apache.subversion.javahl.types.DirEntry;
+import org.apache.subversion.javahl.types.Lock;
 import org.tigris.subversion.svnclientadapter.ISVNDirEntry;
 import org.tigris.subversion.svnclientadapter.ISVNDirEntryWithLock;
 import org.tigris.subversion.svnclientadapter.ISVNLock;
