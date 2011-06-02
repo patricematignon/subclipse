8a7c585b83b5be2faab350f726b3002361884c94
costamojan <costamojan@a34601ff-79c0-0310-90bb-fac03367fbd1>
2011-06-02 09:54:01 +0000

    ~ added the option '--no-auth-cache' in order to disable potential changes to the system credentials Issue #: 995

--- branches/1.6.x/svnClientAdapter/src/commandline/org/tigris/subversion/svnclientadapter/commandline/SvnCommandLine12.java
+++ branches/1.6.x/svnClientAdapter/src/commandline/org/tigris/subversion/svnclientadapter/commandline/SvnCommandLine12.java
@@ -27,107 +27,96 @@ import org.tigris.subversion.svnclientadapter.ISVNNotifyListener;
  */
 public class SvnCommandLine12 extends SvnCommandLine {
 
-	//Constructors
-	SvnCommandLine12(String svnPath,CmdLineNotificationHandler notificationHandler) {
-		super(svnPath,notificationHandler);
-	}	
+    //Constructors
+    SvnCommandLine12(String svnPath,CmdLineNotificationHandler notificationHandler) {
+        super(svnPath,notificationHandler);
+    }   
 
-	/**
-	 * info: Display info about a resource.
-	 * usage: info [PATH [PATH ... ]]
-	 *
-	 *   Print information about PATHs.
-	 *
-	 * Valid options:
-	 *   --targets arg            : pass contents of file ARG as additional args
-	 *   -R [--recursive]         : descend recursively
-	 * 
-	 * @param path
-	 * @return String with the info call result
-	 */
-	String info(String[] target) throws CmdLineException {
+    /**
+     * info: Display info about a resource.
+     * usage: info [PATH [PATH ... ]]
+     *
+     *   Print information about PATHs.
+     *
+     * Valid options:
+     *   --targets arg            : pass contents of file ARG as additional args
+     *   -R [--recursive]         : descend recursively
+     * 
+     * @param path
+     * @return String with the info call result
+     */
+    String info(String[] target) throws CmdLineException {
         if (target.length == 0) {
             // otherwise we would do a "svn info" without args
             return ""; 
         }
         
         setCommand(ISVNNotifyListener.Command.INFO, false);
-        CmdArguments args = new CmdArguments();
-		args.add("info");
+        CmdArguments args = newCmdArguments("info");
         args.addConfigInfo(this.configDir);
         for (int i = 0;i < target.length;i++) {
             args.add(target[i]);
         }
 
-		return execString(args,false);
-	}
+        return execString(args,false);
+    }
 
-	/**
-	 * <p>
-	 * Print the status of working copy files and directories.</p>
-	 *   
-	 * @param path Local path of resource to get status of.
-	 * @param allEntries if false, only interesting entries will be get (local mods and/or out-of-date).
-	 * @param checkUpdates Check for updates on server.
-	 */
-	String statusByStdout(String path[], boolean descend, boolean allEntries, boolean checkUpdates, boolean ignoreExternals) throws CmdLineException {
+    /**
+     * <p>
+     * Print the status of working copy files and directories.</p>
+     *   
+     * @param path Local path of resource to get status of.
+     * @param allEntries if false, only interesting entries will be get (local mods and/or out-of-date).
+     * @param checkUpdates Check for updates on server.
+     */
+    String statusByStdout(String path[], boolean descend, boolean allEntries, boolean checkUpdates, boolean ignoreExternals) throws CmdLineException {
         if (path.length == 0) {
             // otherwise we would do a "svn status" without args
             return ""; 
         }
         setCommand(ISVNNotifyListener.Command.STATUS, false);
-        CmdArguments args = new CmdArguments();
-		args.add("status");
+        CmdArguments args = newCmdArguments("status");
         args.add("-v");
-        if (!allEntries) {
-            args.add("-q");
-        }
-		if (!descend) 
-            args.add("-N");
-		if (checkUpdates)
-			args.add("-u");
-        if (allEntries) {
-        	args.add("--no-ignore"); // disregard default and svn:ignore property ignores
-        }
-        if (ignoreExternals) {
-        	args.add("--ignore-externals");
-        }
-		
+        args.add("-q", !allEntries);
+        args.add("-N", !descend);
+        args.add("-u", checkUpdates);
+        args.add("--no-ignore", allEntries); // disregard default and svn:ignore property ignores
+        args.add("--ignore-externals", ignoreExternals);
+        
         for (int i = 0; i < path.length;i++) { 
             args.add(path[i]);
         }
-		
-		args.addAuthInfo(this.user, this.pass);
+        
+        args.addAuthInfo(this.user, this.pass);
         args.addConfigInfo(this.configDir);
-		return execString(args,false);
-	}
+        return execString(args,false);
+    }
 
-	/**
-	 * Output the content of specified files or URLs with revision and 
-	 * author information in-line.
-	 * @param path
-	 * @param revisionStart
-	 * @param revisionEnd
-	 * @return String with the annotate data
-	 * @throws CmdLineException
-	 */
-	String annotateByStdout(String path,String revisionStart, String revisionEnd) throws CmdLineException {
-		setCommand(ISVNNotifyListener.Command.ANNOTATE, false);
-		CmdArguments args = new CmdArguments();
-		args.add("annotate");
-		args.add("-r");
-		if ((revisionStart != null) && (revisionStart.length() > 0))
-		{
-			args.add(validRev(revisionStart) + ":" + validRev(revisionEnd));	
-		}
-		else
-		{
-			args.add(validRev(revisionEnd));			
-		}
-		args.add(path);
-		args.addAuthInfo(this.user, this.pass);
+    /**
+     * Output the content of specified files or URLs with revision and 
+     * author information in-line.
+     * @param path
+     * @param revisionStart
+     * @param revisionEnd
+     * @return String with the annotate data
+     * @throws CmdLineException
+     */
+    String annotateByStdout(String path,String revisionStart, String revisionEnd) throws CmdLineException {
+        setCommand(ISVNNotifyListener.Command.ANNOTATE, false);
+        CmdArguments args = newCmdArguments("annotate");
+        args.add("-r");
+        if ((revisionStart != null) && (revisionStart.length() > 0))
+        {
+            args.add(validRev(revisionStart) + ":" + validRev(revisionEnd));    
+        }
+        else
+        {
+            args.add(validRev(revisionEnd));            
+        }
+        args.add(path);
+        args.addAuthInfo(this.user, this.pass);
         args.addConfigInfo(this.configDir);
-		return execString(args,false);
-	}
+        return execString(args,false);
+    }
 
 }
